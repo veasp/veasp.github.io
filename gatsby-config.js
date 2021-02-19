eefd414033a9398aa7a23999c37ab3e174c3310e
Roberts Ivanovs <robertsivanovs1999@gmail.com>
2021-02-19 20:29:20 +0200

    Favicon update

--- gatsby-config.js
+++ gatsby-config.js
@@ -68,7 +68,7 @@ module.exports = {
         background_color: `#ffffff`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `content/assets/icon-01.png`,
+        icon: `content/assets/softideas.png`,
       },
     },
     `gatsby-plugin-react-helmet`,
