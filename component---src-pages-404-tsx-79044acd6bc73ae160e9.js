8cdbd257c567565487ad6df7129101b2b8c89f9c
roberts-ivanovs <roberts-ivanovs@users.noreply.github.com>
2021-05-27 17:12:32 +0000

    deployed via Gatsby Publish Action 🎩 for f5122f833d46b9fa403edb2597e3ca0c64268343

---
+++ component---src-pages-404-tsx-79044acd6bc73ae160e9.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{"i6+/":function(t,e,a){"use strict";a.r(e);var n=a("q1tI"),o=a.n(n),i=a("9Dj+"),l=a("H8eV");e.default=function(t){var e=t.data,a=t.location,n=e.site.siteMetadata.title;return o.a.createElement(i.a,{location:a,title:n},o.a.createElement(l.a,{title:"404: Not Found"}),o.a.createElement("h1",null,"Not Found"),o.a.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-tsx-79044acd6bc73ae160e9.js.map
